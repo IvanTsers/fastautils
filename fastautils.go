d02230f1192ad1137975bf56c0cf8fddcf60d5bb
IvanTsers <ivantsers@gmail.com>
2024-11-18 15:23:06 +0100

    added new functions 	DataToUpper() 	ReadAll() 	Concatenate()

--- fastautils.go
+++ fastautils.go
@@ -1,7 +1,9 @@
 package fastautils
 
 import (
+	"fmt"
 	"github.com/evolbioinf/fasta"
+	"os"
 )
 
 // Function Clean removes non-canonical nucleotides from a Sequence (that is, keeps only ATGC/atgc). The function updates the input sequence in place.
@@ -9,12 +11,53 @@ func Clean(s *fasta.Sequence) {
 	d := s.Data()
 	i := 0
 	for _, c := range d {
-		if c == 'A' || c == 'C' || c == 'G' || c == 'T' ||
-			c == 'a' || c == 'c' || c == 'g' || c == 't' {
+		if c == 'A' || c == 'C' ||
+			c == 'G' || c == 'T' ||
+			c == 'a' || c == 'c' ||
+			c == 'g' || c == 't' {
 			d[i] = c
 			i++
 		}
 	}
 	d = d[:i]
 	*s = *fasta.NewSequence(s.Header(), d)
+}
+
+// ReadAll reads all sequences from a file and returns a slice of Sequences.
+func ReadAll(f *os.File) []*fasta.Sequence {
+	sc := fasta.NewScanner(f)
+	var s []*fasta.Sequence
+	for sc.ScanSequence() {
+		s = append(s, sc.Sequence())
+	}
+	f.Close()
+	return s
+}
+
+// Concatenate accepts a slice of Sequences and a sentinel byte. It concatenates the slice into a single Sequence entry, where all headers and data are glued together. The concatenated headers and pieces of data are separated with the sentinel byte, if the latter is not zero.
+func Concatenate(seqSlice []*fasta.Sequence,
+	sentinel byte) (*fasta.Sequence, error) {
+	var err error
+	l := len(seqSlice)
+	switch {
+	case l > 1:
+		h := []byte(seqSlice[0].Header())
+		d := seqSlice[0].Data()
+		for i := 1; i < l; i++ {
+			if sentinel != 0 {
+				h = append(h, sentinel)
+				d = append(d, sentinel)
+			}
+			h = append(h, []byte(seqSlice[i].Header())...)
+			d = append(d, seqSlice[i].Data()...)
+		}
+		cSeq := fasta.NewSequence(string(h), d)
+		return cSeq, err
+	case l == 1:
+		return seqSlice[0], err
+	default:
+		err = fmt.Errorf("fastautils.Concatenate: " +
+			"the input slice is empty\n")
+		return nil, err
+	}
 }
--- fastautils_test.go
+++ fastautils_test.go
@@ -2,6 +2,8 @@ package fastautils
 
 import (
 	"github.com/evolbioinf/fasta"
+	"os"
+	"strconv"
 	"testing"
 )
 
@@ -13,5 +15,66 @@ func TestClean(t *testing.T) {
 	g := string(seq.Data())
 	if g != w {
 		t.Errorf("Clean() want:\n%s\nget:\n%s\n", w, g)
+	}
+}
+func TestReadAll(t *testing.T) {
+	expectedLen := [9]int{0, 0, 0, 5, 70, 140, 700, 1000, 1000}
+	for i := 1; i < 9; i++ {
+		name := "./data/seq" + strconv.Itoa(i) +
+			".fasta"
+		f, err := os.Open(name)
+		if err != nil {
+			t.Errorf("couldn't open %q\n", name)
+		}
+		seqSlice := ReadAll(f)
+		w := expectedLen[i-1]
+		for entry, seq := range seqSlice {
+			g := len(seq.Data())
+			if g != w {
+				t.Errorf("seq%d, entry %d - want: %d\nget: %d",
+					i, entry, w, g)
+			}
+		}
+	}
+}
+func TestConcatenate(t *testing.T) {
+	wantDataLen := [9]int{0, 0, 0, 5, 2*70 + 1,
+		2*140 + 1, 5*700 + 4, 5*1000 + 4}
+	wantHeaders := [9]string{"",
+		"",
+		"seq3",
+		"Rand_1; G/C=0.20",
+		"Rand_1; G/C=0.41|Rand_2; G/C=0.54",
+		"Rand_1; G/C=0.50|Rand_2; G/C=0.50",
+		"Rand_1; G/C=0.50|Rand_2; G/C=0.50|Rand_3; " +
+			"G/C=0.50|Rand_4; G/C=0.50|Rand_5; G/C=0.50",
+		"Rand_1; G/C=0.50|Rand_2; G/C=0.50|Rand_3; " +
+			"G/C=0.50|Rand_4; G/C=0.50|Rand_5; G/C=0.50",
+		"Rand_1; G/C=0.50|Rand_2; G/C=0.50|Rand_3; " +
+			"G/C=0.50|Rand_4; G/C=0.50|Rand_5; G/C=0.50"}
+	for i := 1; i < 9; i++ {
+		name := "./data/seq" + strconv.Itoa(i) +
+			".fasta"
+		f, err := os.Open(name)
+		if err != nil {
+			t.Errorf("couldn't open %q\n", name)
+		}
+		seqSlice := ReadAll(f)
+		seq, err := Concatenate(seqSlice, '|')
+		if err != nil {
+			if i == 1 {
+				continue
+			}
+		}
+		wl := wantDataLen[i-1]
+		gl := len(seq.Data())
+		if gl != wl {
+			t.Errorf("%s data:\nget:\n%d\nwant:\n%d\n", name, gl, wl)
+		}
+		wh := wantHeaders[i-1]
+		gh := seq.Header()
+		if gh != wh {
+			t.Errorf("%s headers:\nget:\n%s\nwant:\n%s\n", name, gh, wh)
+		}
 	}
 }
